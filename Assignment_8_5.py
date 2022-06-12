32d7003e894e9e3dbf24ab6cff985fadddc6521c
Abdurrahman <free.abdo.sh@gmail.com>
2022-06-13 00:58:05 +0300

    first commit

---
+++ Assignment_8_5.py
@@ -0,0 +1,16 @@
+#mail box parser to list all the sender's emails
+
+fname = input("Enter file name: ")
+if len(fname) < 1 :
+    fname = "mbox-short.txt"
+
+fh = open(fname)
+count = 0
+
+for line in fh:
+    line = line.rstrip()
+    if line.startswith("From "):
+        line = line.split()
+        print(line[1])
+        count = count + 1
+print("There were", count, "lines in the file with From as the first word")
